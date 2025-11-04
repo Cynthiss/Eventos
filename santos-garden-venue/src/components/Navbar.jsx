e795f64afa3806c8502dbddd5924de0ba6df0246
Michelle Santos <125904497+Cynthiss@users.noreply.github.com>
2025-11-04 11:23:23 -0600

    Components
    Creacion de la carpeta de componentes, y cambios en el app y en el main

---
+++ santos-garden-venue/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Link, NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+      <Link className="navbar-brand" to="/">Santos Garden Venue</Link>
+
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav"
+        aria-controls="nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="nav">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item"><NavLink className="nav-link" to="/">Inicio</NavLink></li>
+          <li className="nav-item"><NavLink className="nav-link" to="/events">Eventos</NavLink></li>
+          <li className="nav-item"><NavLink className="nav-link" to="/contact">Contacto</NavLink></li>
+          <li className="nav-item"><NavLink className="nav-link" to="/admin">Admin</NavLink></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
